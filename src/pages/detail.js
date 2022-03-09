c1b575661d62d5660768aba6775b9ba868094a9c
KeithHello <41417698+KeithHello@users.noreply.github.com>
2022-03-10 00:41:33 +0900

    詳細画面作成

--- src/pages/detail.js
+++ src/pages/detail.js
@@ -1,8 +1,9 @@
-import { Form } from 'antd';
+import { Button, Form, Input } from 'antd';
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import { Header } from '../component/header';
+import './detail.scss'
 
 export default function Detail() {
     const {id} = useParams();
@@ -10,10 +11,11 @@ export default function Detail() {
     const [index, setIndex] = useState(-1);
 
     useEffect(()=>{
-        axios.get('detail.json',{"abc":id,"hhh":2}).then((response)=>{
+        axios.get('detail.json',{id}).then((response)=>{
             let data = response.data
             if (data.code === 0) {
-                setData(data)
+                console.log(data.data)
+                setData(data.data)
             }
 
             return (()=>{})
@@ -26,10 +28,82 @@ export default function Detail() {
                 <h1>予約管理</h1>
                 <Header index={index} setIndex={setIndex}/>
                 <div className='wrap'>
-                    <Form>
-                        <Form.Item>
-                            
+                    <Form
+                        name="basic"
+                        layout="vertical"
+                        className='form'
+                        initialValues={{
+                          remember: true,
+                          title: data.title,
+                        }}
+                    >
+                        <Form.Item
+                            className="item"
+                            label="タイトル"
+                            name="title"
+                        >
+                            <Input />
                         </Form.Item>
+
+                        <Form.Item
+                            label="予約番号"
+                            name="reservationNumber"
+                        >
+                            <Input defaultValue={data.reservationNumber}/>
+                        </Form.Item>
+
+                        <Form.Item
+                            label="受け種別名"
+                            name="aimOfMeeting"
+                        >
+                            <Input defaultValue={data.aimOfMeeting}/>
+                        </Form.Item>
+
+                        <Form.Item
+                            label="担当者"
+                            name="personInCharge"
+                        >
+                            <Input defaultValue={data.personInCharge}/>
+                        </Form.Item>
+
+                        <Form.Item
+                            label="参加者"
+                            name="name"
+                        >
+                            <Input defaultValue={data.name}/>
+                        </Form.Item>
+
+                        <Form.Item
+                            label="会議室"
+                            name="meetingRoom"
+                        >
+                            <Input defaultValue={data.meetingRoom}/>
+                        </Form.Item>
+
+                        <Form.Item
+                            label="予約日時開始"
+                            name="reservationDateStart"
+                        >
+                            <Input defaultValue={data.reservationDateStart}/>
+                        </Form.Item>
+
+                        <Form.Item
+                            label="予約日時終了"
+                            name="reservationDateEnd"
+                        >
+                            <Input defaultValue={data.reservationDateEnd}/>
+                        </Form.Item>
+
+                        <Form.Item
+                            label="備考"
+                            name="note"
+                        >
+                            <Input defaultValue={data.note}/>
+                        </Form.Item>
+
+                        <Button type="primary" htmlType="submit">
+                            Submit
+                        </Button>
                     </Form>
                     <Link to="/" className='nav-item'>back to index</Link>
                 </div>
